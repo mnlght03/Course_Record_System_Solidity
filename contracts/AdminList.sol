1d605ef8cdc91c5147c8b1543a1a77109af2f2a7
egor <e.morozov@g.nsu.ru>
2022-11-27 17:58:12 +0700

    added addAdmin and deleteAdmin fuctions to AdminList.sol

--- contracts/AdminList.sol
+++ contracts/AdminList.sol
@@ -6,5 +6,22 @@ import "./Admin.sol";
 
 contract AdminList {
   Administrator[] public admins;
-  mapping (address => uint) adminsIdMapping;
+  mapping (address => uint) public adminId;
+
+  function addAdmin(address newAdmin) public {
+    require(newAdmin != address(0));
+    admins.push(new Administrator(newAdmin, admins.length + 1));
+    adminId[newAdmin] = admins.length;
+  }
+
+  function getAdminIdx(address admin) public view returns (uint) {
+    require(admin != address(0));
+    require(adminId[admin] > 0, "AdminList.getAdminIdx: admin doesn't exist");
+    return adminId[admin] - 1;
+  }
+
+  function deleteAdmin(address admin) public {
+    delete admins[getAdminIdx(admin)];
+    delete adminId[admin];
+  }
 }
