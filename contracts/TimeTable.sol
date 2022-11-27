8d70166f1d5055dc1af087c1ac50d31e974e2857
egor <e.morozov@g.nsu.ru>
2022-11-27 20:53:15 +0700

    fixed uint to enum conversions in TimeTable.sol

--- contracts/TimeTable.sol
+++ contracts/TimeTable.sol
@@ -11,23 +11,23 @@ contract TimeTable {
 
   constructor() {}
 
-  function insertCourse(uint _courseId, DayOfTheWeek day, NumberOfLesson time) public {
-    table[day][time] = _courseId;
+  function insertCourse(uint _courseId, uint8 day, uint8 numberOfLesson) public {
+    table[DayOfTheWeek(day)][NumberOfLesson(numberOfLesson)] = _courseId;
   }
 
-  function deleteCourse(DayOfTheWeek day, NumberOfLesson time) public {
-    delete table[day][time];
+  function deleteCourse(uint8 day, uint8 numberOfLesson) public {
+    delete table[DayOfTheWeek(day)][NumberOfLesson(numberOfLesson)];
   }
 
-  function getCourseId(DayOfTheWeek day, NumberOfLesson time) public view returns (uint) {
-    return table[day][time];
+  function getCourseId(uint8 day, uint8 numberOfLesson) public view returns (uint) {
+    return table[DayOfTheWeek(day)][NumberOfLesson(numberOfLesson)];
   }
 
   function getWholeTable() public view returns (uint[7][9] memory) {
     uint[7][9] memory entries;
-    for (uint i = 0; i < 7; i++) {
-      for (uint j = 1; j < 9; j++) {
-        entries[i][j] = getCourseId(DayOfTheWeek(i), NumberOfLesson(j));
+    for (uint8 i = 0; i < 7; i++) {
+      for (uint8 j = 1; j < 9; j++) {
+        entries[i][j] = getCourseId(i, j);
       }
     }
     return entries;
@@ -37,11 +37,11 @@ contract TimeTable {
     uint[7][9] memory entries;
     if (courses.length == 0)
       return entries;
-    for (uint i = 0; i < 7; i++) {
-      for (uint j = 1; j < 9; j++) {
+    for (uint8 i = 0; i < 7; i++) {
+      for (uint8 j = 1; j < 9; j++) {
         entries[i][j] = 0;
-        if (isIncluded(getCourseId(DayOfTheWeek(i), NumberOfLesson(j)), courses))
-          entries[i][j] = getCourseId(DayOfTheWeek(i), NumberOfLesson(j));
+        if (isIncluded(getCourseId(i, j), courses))
+          entries[i][j] = getCourseId(i, j);
       }
     }
     return entries;
