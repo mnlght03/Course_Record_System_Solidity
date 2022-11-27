412a35248a1e6ef3d92f3a5290c5961492190b0b
egor <e.morozov@g.nsu.ru>
2022-11-27 21:05:13 +0700

    added hardhat to project

---
+++ hardhat.config.js
@@ -0,0 +1,26 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+    }
+  },
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+  mocha: {
+    timeout: 40000
+  }
+};
